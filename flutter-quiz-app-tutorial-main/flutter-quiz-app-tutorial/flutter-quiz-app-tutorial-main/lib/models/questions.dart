39884da5b40924f0696203e36e49e451f8135af4
ballkub <janeraweepun.jade@gmail.com>
2024-02-08 13:23:47 +0700

    first commit

---
+++ flutter-quiz-app-tutorial-main/flutter-quiz-app-tutorial/flutter-quiz-app-tutorial-main/lib/models/questions.dart
@@ -0,0 +1,105 @@
+import 'question.dart';
+
+const List<Question> questions = [
+  Question(
+    question: 'เมืองหลวงขอประเทศไทยคืออะไร?',
+    correctAnswerIndex: 0,
+    options: [
+      'a) กรุงเทพมหานคร',
+      'b) เชียงใหม่',
+      'c) พัทยา',
+      'd) พัทลุง',
+    ],
+  ),
+  Question(
+    question: '2.เจ้าของ facebook คือใคร?',
+    correctAnswerIndex: 1,
+    options: [
+      'a) อัลเบิร์ต ไอน์สไตน์',
+      'b) มาร์ก ซักเคอร์เบิร์ก',
+      'c) เอราทอสเทนีส',
+      'd) คาร์ล ฟรีดริช เกาส์',
+    ],
+  ),
+  Question(
+    question: '3.บิดาแห่งวิชาวิทยาศาสตร์คือใคร?',
+    correctAnswerIndex: 0,
+    options: [
+      'a) อัลเบิร์ต ไอน์สไตน์',
+      'b) มาร์ก ซักเคอร์เบิร์ก',
+      'c) เอราทอสเทนีส',
+      'd) คาร์ล ฟรีดริช เกาส์',
+    ],
+  ),
+  Question(
+    question: '4.บิดาแห่งคณิตศาสตร์คือใคร',
+    correctAnswerIndex: 3,
+    options: [
+      'a) อัลเบิร์ต ไอน์สไตน์',
+      'b) มาร์ก ซักเคอร์เบิร์ก',
+      'c) เอราทอสเทนีส',
+      'd) คาร์ล ฟรีดริช เกาส์',
+    ],
+  ),
+  Question(
+    question: '5.บิดาแห่งภูมิศาสตร์คือใคร',
+    correctAnswerIndex: 2,
+    options: [
+      'a) อัลเบิร์ต ไอน์สไตน์',
+      'b) มาร์ก ซักเคอร์เบิร์ก',
+      'c) เอราทอสเทนีส',
+      'd) คาร์ล ฟรีดริช เกาส์',
+    ],
+  ),
+  Question(
+    question: '6.อาหารประจําชาติไทยคืออะไร',
+    correctAnswerIndex: 3,
+    options: [
+      'a) พิซซ่า',
+      'b) มัสมั่น',
+      'c) ต้มยำกุ้ง',
+      'd) ซูชิ',
+    ],
+  ),
+  Question(
+    question: '7.อาหารประจําชาติอิตาลีคืออะไร',
+    correctAnswerIndex: 0,
+    options: [
+      'a) พิซซ่า',
+      'b) มัสมั่น',
+      'c) ต้มยำกุ้ง',
+      'd) ซูชิ',
+    ],
+  ),
+  Question(
+    question: '8.ผลลัพธ์ไดมีค่ามากที่สุด',
+    correctAnswerIndex: 1,
+    options: [
+      'a) 100*100',
+      'b) 100^2',
+      'c) 100*50',
+      'd) 1000/10',
+    ],
+  ),
+  Question(
+    question: '9.สกุลเงินไทยคืออะไร',
+    correctAnswerIndex: 2,
+    options: [
+      'a) หยวน',
+      'b) ดอลลาร์',
+      'c) บาท',
+      'd) เยน',
+    ],
+  ),
+  Question(
+    question:
+        '10.สกุลเงินญี่ปุ่นคืออะไร',
+    correctAnswerIndex: 3,
+    options: [
+      'a) หยวน',
+      'b) ดอลลาร์',
+      'c) บาท',
+      'd) เยน',
+    ],
+  )
+];
